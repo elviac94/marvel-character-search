8bd23d3436900e1ae9dd593883930f24cb551c26
elviac94 <elvi_ac94@hotmail.com>
2020-10-11 14:07:18 +0200

    generate docs folder

---
+++ docs/precache-manifest.2c59b26e63f968b011265099919a7e45.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0333aa64226d16af54b0dbd54f1eecbd",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a209bdc1728c7bd4c3c9",
+    "url": "./static/css/main.63b3d101.chunk.css"
+  },
+  {
+    "revision": "600dc2a363e96a22937e",
+    "url": "./static/js/2.1e7d32d8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.1e7d32d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a209bdc1728c7bd4c3c9",
+    "url": "./static/js/main.e90daea3.chunk.js"
+  },
+  {
+    "revision": "e3d426d2cac9f6ba4718",
+    "url": "./static/js/runtime-main.9f499c19.js"
+  }
+]);
